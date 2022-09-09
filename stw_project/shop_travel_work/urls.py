57cc9eb3a02c0ccc9b6ca5e3795bdb1c72ec4aa4
Patrick Knight <pfknight2@gmail.com>
2022-09-09 14:29:43 -0400

    added permissions for user content control

--- stw_project/shop_travel_work/urls.py
+++ stw_project/shop_travel_work/urls.py
@@ -5,16 +5,15 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-  path('', views.LocationList.as_view(), name='location-list'),
-  path('locations/<int:pk>', views.LocationDetail.as_view(), name='location-detail'),
+  path('locations', views.LocationListView.as_view({'get': 'list', 'post': 'create'}), name='location-list'),
+  path('locations/<int:pk>', views.LocationDetailSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='location-detail'),
+  # Local Fare paths
   path('localfare', views.LocalFareList.as_view(), name='localfare-list'),
-  path('localfare/<int:pk>', views.LocalFareDetail.as_view(), name='localfare-detail'),
+  path('localfare/<int:pk>', views.LocalFareDetailSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='localfare-detail'),
+  # Local Item paths
   path('localitems', views.LocalItemList.as_view(), name='localitem-list'),
-  path('localitems/<int:pk>', views.LocalItemDetail.as_view(), name='localitem-detail'),
-  path('locations/posts', views.LocationPostList.as_view(), name='locationpost-list'),
-  path('locations/posts/<int:pk>', views.LocationPostDetail.as_view(), name='locationpost-detail'),
-  #editing these to make them have auth.
-  # path('users', views.UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
-  # path('users/<int:pk>', views.UserDetailViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user-detail'),
-  # path('obtain', obtain_auth_token)
+  path('localitems/<int:pk>', views.LocalItemDetailSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='localitem-detail'),
+  # Location Blog Post paths
+  path('locations/posts', views.LocationPostListView.as_view({'get': 'list', 'post': 'create'}), name='locationpost-list'),
+  path('locations/posts/<int:pk>', views.LocationPostDetailSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='locationpost-detail'),
 ]
--- stw_project/shop_travel_work/views.py
+++ stw_project/shop_travel_work/views.py
@@ -4,43 +4,159 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
 from .custompermissions import IsOwnerAndAuthenticated
 from shop_travel_work.serializers import LocalFareSerializer, LocationSerializer, LocalItemSerializer, LocationPostSerializer
-from accounts.serializers import UserSerializer
+# from accounts.serializers import UserSerializer
 from .models import Location, LocalFare, LocalItem, LocationPost
-from accounts.models import User
+# from accounts.models import User
 
-# Create your views here.
-class LocationList(generics.ListCreateAPIView):
+###########################
+## - Location Listings - ##
+###########################
+
+class LocationListView(viewsets.ModelViewSet):
   queryset = Location.objects.all()
   serializer_class = LocationSerializer
+  permission_classes_by_action = {'list': [AllowAny], 'create': [IsAdminUser]}
 
-class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+  def list(self, request, *args, **kwargs):
+    return super(LocationListView, self).list(request, *args, **kwargs)
+
+  def create(self, request, *args, **kwargs):
+    return super(LocationListView, self).create(request, *args, **kwargs)
+
+  def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
+
+class LocationDetailSet(viewsets.ModelViewSet):
   queryset = Location.objects.all()
   serializer_class = LocationSerializer
+  permission_classes_by_action = {'retrieve': [AllowAny], 'update': [IsAdminUser], 'destroy': [IsAdminUser]}
+  
+  def retrieve(self, request, *args, **kwargs):
+    return super(LocationDetailSet, self).retrieve(request, *args, **kwargs)
+  
+  def update(self, request, *args, **kwargs):
+    return super(LocationDetailSet, self).update(request, *args, **kwargs)
+
+  def destroy(self, request, *args, **kwargs):
+    return super(LocationDetailSet, self).destroy(request, *args, **kwargs)
+
+  def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
+
+##########################
+## - Local Fare Posts - ##
+##########################
 
 class LocalFareList(generics.ListCreateAPIView):
   queryset = LocalFare.objects.all()
   serializer_class = LocalFareSerializer
 
-class LocalFareDetail(generics.RetrieveUpdateDestroyAPIView):
+class LocalFareDetailSet(viewsets.ModelViewSet):
   queryset = LocalFare.objects.all()
   serializer_class = LocalFareSerializer
+  permission_classes_by_action = {'retrieve': [IsAuthenticated], 'update': [IsOwnerAndAuthenticated], 'destroy': [IsOwnerAndAuthenticated]}
+
+  def retrieve(self, request, *args, **kwargs):
+    return super(LocalFareDetailSet, self).retrieve(request, *args, **kwargs)
+  
+  def update(self, request, *args, **kwargs):
+    return super(LocalFareDetailSet, self).update(request, *args, **kwargs)
+
+  def destroy(self, request, *args, **kwargs):
+    return super(LocalFareDetailSet, self).destroy(request, *args, **kwargs)
+
+  def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
+
+##########################
+## - local Item Posts - ##
+##########################
 
 class LocalItemList(generics.ListCreateAPIView):
   queryset = LocalItem.objects.all()
   serializer_class = LocalItemSerializer
 
-class LocalItemDetail(generics.RetrieveUpdateDestroyAPIView):
+class LocalItemDetailSet(viewsets.ModelViewSet):
   queryset = LocalItem.objects.all()
   serializer_class = LocalItemSerializer
-  permission_classes = [IsOwnerAndAuthenticated]
+  permission_classes_by_action = {'retrieve': [IsAuthenticated], 'update': [IsOwnerAndAuthenticated], 'destroy': [IsOwnerAndAuthenticated]}
 
-class LocationPostList(generics.ListCreateAPIView):
+  def retrieve(self, request, *args, **kwargs):
+    return super(LocalItemDetailSet, self).retrieve(request, *args, **kwargs)
+  
+  def update(self, request, *args, **kwargs):
+    return super(LocalItemDetailSet, self).update(request, *args, **kwargs)
+
+  def destroy(self, request, *args, **kwargs):
+    return super(LocalItemDetailSet, self).destroy(request, *args, **kwargs)
+
+  def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
+
+########################
+## - Location Posts - ##
+########################
+
+class LocationPostListView(viewsets.ModelViewSet):
   queryset = LocationPost.objects.all()
   serializer_class = LocationPostSerializer
+  permission_classes_by_action = {'list': [AllowAny], 'create': [IsAuthenticated]}
 
-class LocationPostDetail(generics.RetrieveUpdateDestroyAPIView):
+  def list(self, request, *args, **kwargs):
+    return super(LocationPostListView, self).list(request, *args, **kwargs)
+
+  def create(self, request, *args, **kwargs):
+    return super(LocationPostListView, self).create(request, *args, **kwargs)
+
+  def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
+
+class LocationPostDetailSet(viewsets.ModelViewSet):
   queryset = LocationPost.objects.all()
   serializer_class = LocationPostSerializer
+  permission_classes_by_action = {'retrieve': [IsAuthenticated], 'update': [IsOwnerAndAuthenticated], 'destroy': [IsOwnerAndAuthenticated]}
+
+  def retrieve(self, request, *args, **kwargs):
+    return super(LocationPostDetailSet, self).retrieve(request, *args, **kwargs)
+  
+  def update(self, request, *args, **kwargs):
+    return super(LocationPostDetailSet, self).update(request, *args, **kwargs)
+
+  def destroy(self, request, *args, **kwargs):
+    return super(LocationPostDetailSet, self).destroy(request, *args, **kwargs)
+
+  def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
 
 #######################
 ## obsolete - moved user stuff to accounts, delete this if works without it here.
