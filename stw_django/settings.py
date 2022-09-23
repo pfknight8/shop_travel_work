931b9ca8d9b89c690b49d4534cfcd7c03e40889e
Patrick Knight <pfknight2@gmail.com>
2022-09-23 11:00:21 -0400

    try heroku deploy again

--- stw_django/settings.py
+++ stw_django/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# For heroku, otherwise set manually to True/False
+DEBUG = True if os.environ['MODE'] == 'dev' else False
 
 ALLOWED_HOSTS = ['*',]
 
