2b44ccd40bad2712c9f2ea48dfe14a1af266c1b6
Patrick Knight <pfknight2@gmail.com>
2022-09-23 10:48:18 -0400

    user can deactivate now

--- accounts/views.py
+++ accounts/views.py
@@ -42,7 +42,7 @@ class UserDetailViewSet(viewsets.ModelViewSet):
     return super(UserDetailViewSet, self).update(request, *args, **kwargs)
 
   def destroy(self, request, *args, **kwargs):
-    user = self.get_object()
+    user = request.user
     user.active = False
     user.save()
     response = HttpResponse("User Deleted")
